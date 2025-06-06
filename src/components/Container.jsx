3a483b944febae62f54a3e56d3c70eed44426338
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-06-06 15:46:10 +0530

    Card 2 and button

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,8 @@
-const Container = ()=>{
- return (<div className="card" style={{width: "18rem"}}>
+const Container = ({children})=>{
+ return (<div className="card" style={{width: "70%"}}>
   
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    {children}
   </div>
 </div>)
 }
