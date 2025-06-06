42846394bd42c022dde57ee71cebbfa11dc8e30a
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-06-06 17:33:57 +0530

    Dispatch and Increment/Decrement ,earlier Subscribe

--- src/components/DisplayCounter.jsx
+++ src/components/DisplayCounter.jsx
@@ -1,7 +1,11 @@
+import {useSelector} from "react-redux";
+
 const DisplayCounter = () => {
+    const counter = useSelector(store=>store.counter);
   return (
     <>
-      <p className="lead mb-4">Counter current value</p>
+   
+      <p className="lead mb-4">Counter current value:{counter}</p>
     </>
   );
 };
