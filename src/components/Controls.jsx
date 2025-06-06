a0a90975101ba88e3b29e72217e5389c49c24163
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-06-06 18:57:44 +0530

    Add and Subtract and Increment Decrement functionality

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,7 +1,10 @@
 import {useDispatch} from 'react-redux';
+import {useRef} from "react";
 const Controls = ()=>{
 
+
 const dispatch=useDispatch();
+const  inputElement =useRef();
 
 const handleIncrement=()=>{
     dispatch({type:"INCREMENT"});
@@ -9,16 +12,34 @@ const handleIncrement=()=>{
 const handleDecrement=()=>{
     dispatch({type:"DECREMENT"});
 }
-
+const handleAdd=()=>{
+    dispatch({type:"ADD",payload:{
+        num:inputElement.current.value,
+    }});
+    inputElement.current.value="";
+}
+const handleSubtract=()=>{
+    dispatch({type:"SUB",payload:{
+        num: inputElement.current.value,
+    }});
+    inputElement.current.value="";
+}
 
 
 
 
  return (
+    <>
     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                        <button type="button" className="btn btn-primary" onClick={handleIncrement}>+1</button>
+<button type="button" className="btn btn-primary" onClick={handleIncrement}>+1</button>
 <button type="button" className="btn btn-success" onClick={handleDecrement}>-1</button> 
-                         </div> 
+  </div>
+     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+<input type="text"  ref={inputElement} placeholder="Enter number" className="number-input"/>
+<button type="button" className="btn btn-info" onClick={handleAdd}>Add</button> 
+<button type="button" className="btn btn-danger" onClick={handleSubtract}>Subtract</button> 
+  </div>
+  </> 
  )
 }
 export default Controls;
